7ef9ea99729e0b2d58d6cb0cc0dea24b7ea5aa63
sabiinajm <sabiinajm@gmail.com>
2025-01-11 11:23:16 +0400

    vercel deployment issues fix

--- src/layouts/RegLayout.jsx
+++ src/layouts/RegLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
-import RegHeader from "../components/header/RegHeader"
-import RegFooter from "../components/footer/RegFooter"
+import RegHeader from "../components/header/RegHeader.jsx"
+import RegFooter from "../components/footer/RegFooter.jsx"
 
 function RegLayout() {
     return (
